2c1491cc3986ee596694ddc791bb1dcd7a3f7162
K00195112 <K00195112@MY_8A106_15.student.litdom.lit.ie>
2016-04-21 10:29:36 +0100

    declaration of x

--- src/main/java/com/mycompany/mavenunittestproject/UtilityClass.java
+++ src/main/java/com/mycompany/mavenunittestproject/UtilityClass.java
@@ -2,36 +2,29 @@ package com.mycompany.mavenunittestproject;
 
 import java.math.BigInteger;
 
-public class UtilityClass 
-{
+public class UtilityClass {
 
-    private UtilityClass () { }
-
-
-    public static String concatWords(String... words) 
-    {
+    private UtilityClass() {
+    }
+    int x = 0;
+    public static String concatWords(String... words) {
         StringBuilder buf = new StringBuilder();
-        for (String word : words) 
-{
+        for (String word : words) {
             buf.append(word);
         }
         return buf.toString();
     }
 
-  public static String computeFactorial(int number) 
-                                               throws IllegalArgumentException 
-  {
-        if (number < 1) 
-{
+    public static String computeFactorial(int number)
+            throws IllegalArgumentException {
+        if (number < 1) {
             throw new IllegalArgumentException("zero or negative parameter (" + number + ')');
         }
 
         BigInteger factorial = new BigInteger("1");
-        for (int i = 2; i <= number; i++) 
-{
+        for (int i = 2; i <= number; i++) {
             factorial = factorial.multiply(new BigInteger(String.valueOf(i)));
         }
         return factorial.toString();
     }
 }
-
